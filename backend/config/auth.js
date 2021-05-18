8710da01a74d7b44b27207a4a99eb267cad5d1b1
Emilian Cismas <jackkan009@gmail.com>
2021-05-18 11:27:16 +0100

    auth with jwt bcrypt  added

---
+++ backend/config/auth.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "emillion7-secret-key"
+  };
--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,5 +2,12 @@ module.exports = {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "emilian77",
-    DB: "groupomania"
+    DB: "groupomania",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   };
